800d559b9cc848a350d464a0b889b5dc10b4a427
Artem Ganev <a.a.ganev@gmail.com>
2022-03-31 18:21:40 +0300

    Add `dotenv`

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-var host = process.env.HOST || 'localhost';
-var port = process.env.PORT || 3001;
+require('dotenv').config();
 
-var cors_proxy = require('cors-anywhere');
-cors_proxy
-  .createServer({
-    originWhitelist: ['http://localhost:3000'], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2'],
-    handleInitialRequest: function (req, res, location) {
-      console.log(req.method, location.href);
-      console.log(res);
-    },
-  })
-  .listen(port, host, function () {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-  });
+function main() {
+  var host = process.env.HOST || 'localhost';
+  var port = process.env.PORT || 3001;
+  var cors_proxy = require('cors-anywhere');
+
+  cors_proxy
+    .createServer({
+      originWhitelist: ['http://localhost:3000'], // Allow all origins
+      requireHeader: ['origin', 'x-requested-with'],
+      removeHeaders: ['cookie', 'cookie2'],
+      handleInitialRequest: function (req, res, location) {
+        console.log(req.method, location.href);
+        console.log(res);
+      },
+    })
+    .listen(port, host, function () {
+      console.log('Running CORS Anywhere on ' + host + ':' + port);
+    });
+}
+
+main();
